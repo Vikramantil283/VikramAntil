d6533bd0bba2913407ed94552d01e597523cd7c0
vikram <vikramantil283@gmail.com>
2021-05-28 15:37:34 +0530

    3.commit

--- app/src/main/java/com/example/my1/BaseActivity.kt
+++ app/src/main/java/com/example/my1/BaseActivity.kt
@@ -1,8 +1,27 @@
 package com.example.my1
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
-class BaseActivity : AppCompatActivity(){
+abstract class BaseActivity : AppCompatActivity(){
+
+private val tag= "BaseActivity"
+
+    override fun setContentView(layoutID : Int){
+        setContentView(layoutID)
+       // super.onNightModeChanged()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        initUI()
+        initListener()
+        setData()
+    }
+
+    abstract fun initUI()
+
+    abstract fun initListener()
+
+    abstract fun setData()
+
 
 
 }
